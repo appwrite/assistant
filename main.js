781a5dcb4be0aeab94ab73925c04338b076c841a
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-13 14:25:06 +0100

    small updates

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import { Configuration, OpenAIApi } from "openai-edge";
 import { getChain, search_index } from "./embeddings.js";
 
 const app = express();
@@ -14,17 +13,11 @@ app.use(bodyParser.raw({ inflate: true, type: "*/*" }));
 
 const port = 3003;
 
-// Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(config);
-
 const template = (
   prompt
 ) => `You are an AI chat bot trained on Appwrite Docs. You need to help developers answer Appwrite related
 question only. You will be given an input and you need to respond with the appropriate answer from the
-reference docs. For each question show code examples when applicable. Answer in atleast 200 words.
+reference docs. For each question show code examples when applicable.
 ${prompt}`;
 
 app.post("/", async (req, res) => {
